4a4ebd258184e806d4ec7ddccfc4a7374293a27a
Ulisses Caon <ulissescaon@gmail.com>
2018-01-03 13:58:00 -0200

    Add objects to jsx

--- module1/public/scripts/app.js
+++ module1/public/scripts/app.js
@@ -2,11 +2,68 @@
 
 console.log('App.js is running');
 
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Testing'
+};
+
 var template = React.createElement(
-  'h2',
+  'div',
   null,
-  'Hello'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
+
+var user = {
+  name: 'Ulisses',
+  age: 23,
+  location: 'Porto União'
+};
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: ',
+    user.location
+  )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- module1/src/app.js
+++ module1/src/app.js
@@ -1,6 +1,35 @@
 console.log('App.js is running')
 
-let template = <h2>Hello</h2>
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Testing'
+}
+
+let template = (
+  <div>
+    <h1>{ app.title }</h1>
+    <p>{ app.subtitle }</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+)
+
+let user = {
+  name: 'Ulisses',
+  age: 23,
+  location: 'Porto União'
+}
+
+let templateTwo = (
+  <div>
+    <h1>{ user.name }</h1>
+    <p>Age: { user.age }</p>
+    <p>Location: { user.location }</p>
+  </div>
+)
+
 let appRoot = document.getElementById('app')
 
 ReactDOM.render(template, appRoot)
