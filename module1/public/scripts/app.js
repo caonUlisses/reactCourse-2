40f1f18c32360aa6daac5d0d75f8f9cfbd200dd2
Ulisses Caon <ulissescaon@gmail.com>
2018-01-03 20:14:04 -0200

    Finish counter

--- module1/public/scripts/app.js
+++ module1/public/scripts/app.js
@@ -40,38 +40,59 @@ var template = React.createElement(
   )
 );
 
-var user = {
-  name: 'Ulisses',
-  age: 23,
-  location: 'Porto União'
+var count = 0;
+
+var addOne = function addOne() {
+  count++;
+  renderCounterApp();
 };
 
-function getLocation(location) {
-  return React.createElement(
-    'p',
-    null,
-    'Location: ',
-    location
-  ) || null;
-}
+var minusOne = function minusOne() {
+  count--;
+  renderCounterApp();
+};
 
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    user.name ? user.name : 'Anonymous'
-  ),
-  user.age && user.age >= 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
-  ),
-  getLocation(user.location)
-);
+var reset = function reset() {
+  count = 0;
+  renderCounterApp();
+};
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count,
+      ' '
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'Reset'
+    ),
+    count >= 10 && React.createElement(
+      'p',
+      null,
+      'olha s\xF3'
+    )
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
