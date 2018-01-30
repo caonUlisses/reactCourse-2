ff26aad518532fdae602a005a041a6cd32d724aa
Ulisses Caon <ulissescaon@gmail.com>
2018-01-30 19:44:41 -0200

    Finish webpack module

--- module1/src/components/IndecisionApp.js
+++ module1/src/components/IndecisionApp.js
@@ -6,17 +6,47 @@ import Header from './Header.js'
 import Action from './Action.js'
 
 export default class IndecisionApp extends React.Component {
-  constructor (props) {
-    super(props)
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
-    this.handlePick = this.handlePick.bind(this)
-    this.handleAddOption = this.handleAddOption.bind(this)
-    this.handleDeleteOption = this.handleDeleteOption.bind(this)
-    this.state = {
+  state = {
       options: []
-    }
   }
 
+  handleDeleteOptions = () => {
+    this.setState(() => ({
+      options: []
+    })
+    )
+  }
+
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option) }))
+  }
+
+  handlePick = () => {
+    const randonNum = Math.floor(Math.random() * this.state.options.length)
+    const option = this.state.options[randonNum]
+    alert(option)
+  }
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value to add item'
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists'
+    }
+
+    this.setState((prevState) => ({
+      options: prevState.options.concat([option])
+    }))
+  }
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options)
+      localStorage.setItem('options', json)
+    }
+  }
+   
   componentDidMount () {
     try {
       const json = localStorage.getItem('options')
@@ -30,42 +60,6 @@ export default class IndecisionApp extends React.Component {
     }
   }
 
-  componentDidUpdate (prevProps, prevState) {
-    if (prevState.options.length !== this.state.options.length) {
-      const json = JSON.stringify(this.state.options)
-      localStorage.setItem('options', json)
-    }
-  }
-
-  handleDeleteOptions () {
-    this.setState(() => ({
-      options: []
-    })
-    )
-  }
-
-  handleDeleteOption (optionToRemove) {
-    this.setState((prevState) => ({
-      options: prevState.options.filter((option) => optionToRemove !== option) }))
-  }
-
-  handlePick () {
-    const randonNum = Math.floor(Math.random() * this.state.options.length)
-    const option = this.state.options[randonNum]
-    alert(option)
-  }
-
-  handleAddOption (option) {
-    if (!option) {
-      return 'Enter valid value to add item'
-    } else if (this.state.options.indexOf(option) > -1) {
-      return 'This option already exists'
-    }
-
-    this.setState((prevState) => ({
-      options: prevState.options.concat([option])
-    }))
-  }
 
   render () {
     const subtitle = 'Put your life in the hands of a computer'
