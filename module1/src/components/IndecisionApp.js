7bba71699a5f2fb17894018592ee088a9747661b
Ulisses Caon <ulissescaon@gmail.com>
2018-02-02 16:43:18 -0200

    Style buttons

--- module1/src/components/IndecisionApp.js
+++ module1/src/components/IndecisionApp.js
@@ -81,6 +81,7 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
+        <div className="container">
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
@@ -97,6 +98,7 @@ export default class IndecisionApp extends React.Component {
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
         />
+        </div>
       </div>
     )
   }
--- module1/src/components/Options.js
+++ module1/src/components/Options.js
@@ -4,7 +4,9 @@ import Option from './Option.js'
 
 const Options = (props) => (
   <div>
-    <button onClick={props.handleDeleteOptions}>Remove all</button>
+    <button
+      className='button button--link'
+      onClick={props.handleDeleteOptions}>Remove all</button>
     { props.options.length === 0 && <p>Please add an option to get started</p> }
     <ol>
       {
