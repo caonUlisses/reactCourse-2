e252ed2b7a6676dad86df74cd4b8e61eba4a971d
Ulisses Caon <ulissescaon@gmail.com>
2018-02-03 15:11:00 -0200

    Design app interface

--- module1/src/components/IndecisionApp.js
+++ module1/src/components/IndecisionApp.js
@@ -86,6 +86,7 @@ export default class IndecisionApp extends React.Component {
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
         />
+        <div className="widget">
         <Options
           options={this.state.options}
           handleDeleteOptions={this.handleDeleteOptions}
@@ -94,6 +95,7 @@ export default class IndecisionApp extends React.Component {
         <AddOption
           handleAddOption={this.handleAddOption}
         />
+        </div>
         <OptionModal 
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
--- module1/src/components/OptionModal.js
+++ module1/src/components/OptionModal.js
@@ -6,12 +6,15 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleClearSelectedOption}
     contentLabel='Selected Option'
+    closeTimeOutMS={400}
+    className='modal'
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p> }
-    <button
+    <h3 className='modal__title'>Selected Option:</h3>
+      {props.selectedOption && <p className='modal__body'>{props.selectedOption}</p> }
+      <button
+        className='button'
       onClick={props.handleClearSelectedOption}
-  >Ok</button>
+    >Ok</button>
   </Modal>
 )
 
